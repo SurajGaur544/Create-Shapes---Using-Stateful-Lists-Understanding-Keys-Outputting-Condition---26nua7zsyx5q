3f6107393edaa8ed3332c799140a0e6b57681342
Suraj Gaur <surajgaur242@gmail.com>
2023-11-13 12:44:00 +0530

    modified some code

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import '../styles/App.css';
+import React, {useState} from 'react';
 const App = () => {
+  const [selectedShape, setSelectedShape] = useState("square");
+  const [shapeList, setShapeList] = useState([]);
+  const onButtonClick = () => {
+    setShapeList([...shapeList, setSelectedShape.toLowerCase()]);
+  };
 
   return (
     <div id="main">
       <div id="shape-creator">
-        
-
+        <select value={selectedShape} 
+        onChange={(e) => setSelectedShape(e.target.value)}>
+          <option id='option-square'>Square</option>
+          <option id='option-circle'>Circle</option>
+        </select>
+        <button onClick={onButtonClick}>Add Shape</button>
       </div>
       <div id="shapes-holder">
+        {shapeList.map((item, index) => (
+          <div key={index} className={item}>
+            {index}
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
